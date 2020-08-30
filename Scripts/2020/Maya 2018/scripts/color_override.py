b91ac5051518f332d8a5b2f226a1e069f11e9b23
Unknown <rp17000@gmail.com>
2020-08-30 03:26:58 -0600

    Slider to change control colors

---
+++ Scripts/2020/Maya 2018/scripts/color_override.py
@@ -0,0 +1,35 @@
+import maya.cmds as cmds
+import re
+
+
+def default_color():
+    '''Changes override color to 0 and disables override'''
+    change_color(1, False)
+
+
+def change_color(index, enabled):
+    '''Sets override color number and enable status'''
+    if(index>32):
+        index = 32
+    index = index-1
+    sels = cmds.ls(sl=True)
+    for sel in sels:
+        shapes = cmds.listRelatives(sel, shapes=True)
+        for shape in shapes:
+            cmds.setAttr("{0}.overrideEnabled".format(shape), enabled)
+            cmds.setAttr("{0}.overrideColor".format(shape), index)
+
+
+def change_color_slider(slider):
+    '''Sets override color from a colorIndexSliderGrp '''
+    # fix name issue created by dockControl
+    slider = re.sub('CSAToolboxWindow', 'MayaWindow', slider)
+
+    # check for slider
+    if(cmds.colorIndexSliderGrp(slider, q=True, exists=True)):
+        # get value from slider
+        value = cmds.colorIndexSliderGrp(slider, q=True, value=True)
+        # set color
+        change_color(value, True)
+    else:
+        print(slider+ ' not found')
--- Scripts/2020/Maya 2018/scripts/toolbox.py
+++ Scripts/2020/Maya 2018/scripts/toolbox.py
@@ -4,6 +4,7 @@
 #============================================
 import maya.cmds as cmds
 import maya.mel as mel
+
 import rename_functions
 import joint_functions
 import auto_rigs
@@ -15,6 +16,7 @@ import create_locator
 import custom_outliner
 import random_placement
 import custom_animation_window
+import color_override
 
 
 def create_my_toolbox(standalone=False):
@@ -31,6 +33,7 @@ def create_my_toolbox(standalone=False):
 	reload(custom_outliner)
 	reload(random_placement)
 	reload(custom_animation_window)
+	reload(color_override)
 
 	# Create window
 	my_window = 'CSAToolboxWindow'
@@ -38,7 +41,7 @@ def create_my_toolbox(standalone=False):
 	my_window = cmds.window(my_window, width=160, height=570)
 
     # Create main column
-	main_column = cmds.columnLayout(p=my_window, adjustableColumn=True, columnAlign='center')
+	main_column = cmds.columnLayout('ColumnLayout', p=my_window, adjustableColumn=True, columnAlign='center')
 
 	# Create header
 	image_row = cmds.rowLayout(p=main_column, nc=2, adjustableColumn=2, columnAlign=(2,'center'), bgc=(1,1,1))
@@ -46,29 +49,90 @@ def create_my_toolbox(standalone=False):
 	my_name = cmds.text(p=image_row, label='Cody Anderson')
 
 	# Create tab layout
-	my_tabs = cmds.tabLayout(p=main_column)
+	my_tabs = cmds.tabLayout('TabLayout', p=main_column)
 	
 	# Tab 1
-	tab_1_column = cmds.columnLayout(p=my_tabs, adjustableColumn=True, columnAlign='center')
-	
-	# Simple modeling tools
+	tab_1_column = cmds.columnLayout('TabColumn1',p=my_tabs, adjustableColumn=True, columnAlign='center')
+
+	# MODELING TOOLS
 	cmds.separator(p=tab_1_column, height=12)
+	other_label = cmds.text(p=tab_1_column, l='Modeling')
+	cmds.separator(p=tab_1_column, height=12)
+
+	# Poly Count
+	poly_count_button = cmds.button(p=tab_1_column, l='Poly Count', ann='Toggle poly count',
+									command=lambda *args: simple_functions.toggle_poly_count())
+
+	cmds.separator(p=tab_1_column, height=12)
+
+	# Simple modeling tools
 	delete_history_button = cmds.button(p=tab_1_column, l='Delete History', ann='Delete history',
 										command=lambda *args: simple_functions.delete_history())
 	freeze_transform_button = cmds.button(p=tab_1_column, l='Freeze Transformations', ann='Freeze all transformations',
 										command=lambda *args: simple_functions.freeze_transformations())
 	center_pivot_button = cmds.button(p=tab_1_column, l='Center Pivot', ann='Center pivot',
 										command=lambda *args: simple_functions.center_pivot())
-	parent_scale_button = cmds.button(p=tab_1_column, l='Parent and Scale Constrain', ann='Parent and scale constrain',
-										command=lambda *args: simple_functions.parent_scale_constrain())
-	
-    # Poly Count
+
 	cmds.separator(p=tab_1_column, height=12)
-	poly_count_button = cmds.button(p=tab_1_column, l='Poly Count', ann='Toggle poly count',
-										command=lambda *args: simple_functions.toggle_poly_count())
-	
+
+	# Rename Functions
+	rename_button = cmds.button(p=tab_1_column, l='Rename', ann='Sequential renamer',
+								command=lambda *args: rename_functions.rename_prompt())
+	search_and_replace_button = cmds.button(p=tab_1_column, l='Search And Replace', ann='Search and replace',
+											command=lambda *args: rename_functions.search_and_replace())
+
+	# RIGGING TOOLS
+	cmds.separator(p=tab_1_column, height=12)
+	other_label = cmds.text(p=tab_1_column, l='Rigging')
+	cmds.separator(p=tab_1_column, height=12)
+
+	# Locator
+	locator_row = cmds.rowLayout(p=tab_1_column, nc=3, adj=3, columnWidth=(1, 62), columnAttach=(1, 'both', 0))
+	create_locator_button = cmds.button(p=locator_row, l='Locators', ann='Create locator(s)',
+										command=lambda *args: create_locator.center_locator(locator_buttons))
+	locator_buttons = cmds.radioCollection(p=locator_row)
+	locator_button_1 = cmds.radioButton(l='Each', ann='Create locator at each selection')
+	locator_button_2 = cmds.radioButton(l='All', ann='Create locator at center of entire selection')
+	cmds.radioCollection(locator_buttons, edit=True, select=locator_button_1)
+
+	# Covert to Joints
+	create_joints_row = cmds.rowLayout(p=tab_1_column, nc=2, adj=2, columnWidth=(1, 110), columnAttach=(1, 'left', 0))
+	create_joints_button = cmds.button(p=create_joints_row, l='Selection to Joints', ann='Places joint at selection',
+									   command=lambda *args: create_joints.convert_to_joints(create_joints_checkbox))
+	create_joints_checkbox = cmds.checkBox(p=create_joints_row, l='Del', ann='Delete selection?', align='right')
+
+	# Place Controls
+	place_controls_button = cmds.button(p=tab_1_column, l='Place Controls', ann='Place controls at selection',
+										command=lambda *args: create_controls.place_controls())
+
+	cmds.separator(p=tab_1_column, height=12)
+
+	# Change color
+	color_slider = cmds.colorIndexSliderGrp(p=tab_1_column, w=80, columnWidth=(1, 60), columnAttach=(1, 'both', 0), adj=2,
+										min=1, max=32, value=1)
+	color_buttons_row = cmds.rowLayout(p=tab_1_column, nc=2, adj=2, columnWidth=(1, 75), columnAttach=(1, 'both', 0))
+	color_set_button = cmds.button(p=color_buttons_row, l='Set Color', ann='Changes color to slider color',
+								   command=lambda *args: color_override.change_color_slider(color_slider))
+	color_default_button = cmds.button(p=color_buttons_row, l='Default Color', ann='Changes color back to default',
+									   command=lambda *args: color_override.default_color())
+
+	cmds.separator(p=tab_1_column, height=12)
+
+	# Contstraints
+	parent_constrain_button = cmds.button(p=tab_1_column, l='Parent Constrain', ann='Parent constrain',
+									  command=lambda *args: cmds.parentConstraint())
+	scale_constrain_button = cmds.button(p=tab_1_column, l='Scale Constrain', ann='Scale constrain',
+									  command=lambda *args: cmds.scaleConstraint())
+	point_constrain_button = cmds.button(p=tab_1_column, l='Point Constrain', ann='Point constrain',
+									 command=lambda *args: cmds.pointConstraint())
+	orient_constrain_button = cmds.button(p=tab_1_column, l='Orient Constrain', ann='Orient constrain',
+									 command=lambda *args: cmds.orientConstraint())
+	pole_vector_constrain_button = cmds.button(p=tab_1_column, l='Pole Vector Constrain', ann='Pole vector constrain',
+									  command=lambda *args: cmds.poleVectorConstraint())
+
+	cmds.separator(p=tab_1_column, height=12)
+
 	#Orient Joint
-	cmds.separator(p=tab_1_column, height=12)
   	orient_joint_row = cmds.rowLayout(p=tab_1_column, nc=2, adj=2, columnWidth=(1, 125), columnAttach=(1, 'both', 0))
 	orient_joint_button = cmds.button(p=orient_joint_row, l='Orient Joint Options', ann='Display orient joint options',
 										command=lambda *args: joint_functions.display_orient_joint_options())
@@ -77,63 +141,60 @@ def create_my_toolbox(standalone=False):
 										command=lambda *args: joint_functions.display_local_axis())
 	orient_channelbox_button = cmds.button(p=tab_1_column, l='Toggle Joint Attributes', ann='Toggles joint attributes',
 										command=lambda *args: joint_functions.show_joint_attr())
-	
-	#Scale Compensation
+
 	cmds.separator(p=tab_1_column, height=12)
+
+	#Scale Compensation
 	scale_comp_label = cmds.text(p=tab_1_column, l='Scale Compensation')
 	scale_comp_row = cmds.rowLayout(p=tab_1_column, nc=2, adj=2, columnWidth=(1, 75), columnAttach=(1, 'both', 0))
 	scale_comp_on_button = cmds.button(parent=scale_comp_row, label='On', annotation='Turn scale compensation on',
 										command=lambda *args: joint_functions.scale_compensate(1))
 	scale_comp_off_button = cmds.button(parent=scale_comp_row, label='Off', annotation='Turn scale compensation off',
 										command=lambda *args: joint_functions.scale_compensate(0))
-	
-	#Covert to Joints
-	cmds.separator(p=tab_1_column, height=12)
-	create_joints_row = cmds.rowLayout(p=tab_1_column, nc=2, adj=2, columnWidth=(1, 110), columnAttach=(1, 'left', 0))
-	create_joints_button = cmds.button(p=create_joints_row, l='Selection to Joints', ann='Places joint at selection',
-										command=lambda *args: create_joints.convert_to_joints(create_joints_checkbox))
-	create_joints_checkbox = cmds.checkBox(p=create_joints_row, l='Del', ann='Delete selection?', align='right')
 
-    #Place Controls
-	place_controls_button = cmds.button(parent=tab_1_column, label='Place Controls', annotation='Place controls at selection', command=lambda *args: create_controls.place_controls())
-	
-	#Locator
-	cmds.separator(height=12, parent=tab_1_column)
-	locator_row = cmds.rowLayout(parent=tab_1_column, numberOfColumns=3, columnWidth=(1, 62), adjustableColumn=2, columnAttach = (1, 'both', 0))
-	create_locator_button = cmds.button(parent=locator_row, label='Locators', annotation='Create locator(s)', command=lambda *args: create_locator.center_locator(locator_buttons))
-	locator_buttons = cmds.radioCollection(parent=locator_row)
-	locator_button_1 = cmds.radioButton(label='Each', annotation='Create locator at each selection')
-	locator_button_2 = cmds.radioButton(label='All', annotation='Create locator at center of entire selection')
-	cmds.radioCollection(locator_buttons, edit=True, select=locator_button_1)
-	
-	#Rename Functions
-	cmds.separator(height=12, parent=tab_1_column)
-	rename_button = cmds.button(parent=tab_1_column, label='Rename', annotation='Sequential renamer', command=lambda *args: rename_functions.rename_prompt())
-	search_and_replace_button = cmds.button(parent=tab_1_column, label='Search And Replace', annotation='Search and replace', command=lambda *args: rename_functions.search_and_replace())
+	cmds.separator(p=tab_1_column, height=12)
 
 	#Auto rigs
-	cmds.separator(height=12, parent=tab_1_column)
-	auto_rigs_row = cmds.rowLayout(parent=tab_1_column, numberOfColumns=4, columnAlign=(2, 'left'))
-	fk_button = cmds.iconTextButton(style='iconAndTextVertical', image1='kinJoint.png', label='FK', annotation='Creates an FK rig. Select first joint of chain then run', parent= auto_rigs_row, command=lambda *args: auto_rigs.simple_fk())
-	broken_fk_button = cmds.iconTextButton(style='iconAndTextVertical', image1='kinJoint.png', label='Broken', annotation='Creates an broken FK rig. Select first joint of chain then run', parent= auto_rigs_row, command=lambda *args: auto_rigs.broken_fk())
-	ik_button = cmds.iconTextButton(style='iconAndTextVertical', image1='kinHandle.png', label='IK', annotation='Creates an IK rig. Select first joint of chain then run', parent= auto_rigs_row, command=lambda *args: auto_rigs.simple_ik())
-	rk_button = cmds.iconTextButton(style='iconAndTextVertical', image1='kinJoint.png', label='RK', annotation='Creates an RK rig. Select first joint of chain then run', parent= auto_rigs_row, command=lambda *args: auto_rigs.simple_rk())
+	auto_rigs_row = cmds.rowLayout(p=tab_1_column, nc=4, columnAlign=(2, 'left'))
+	fk_button = cmds.iconTextButton(p=auto_rigs_row, l='FK', ann='Creates a FK rig. Select first joint',
+									style='iconAndTextVertical', image1='kinJoint.png',
+									command=lambda *args: auto_rigs.simple_fk())
+	broken_fk_button = cmds.iconTextButton(p=auto_rigs_row, l='Broken', ann='Creates a broken FK rig. Select first joint',
+									style='iconAndTextVertical', image1='kinJoint.png',
+									command=lambda *args: auto_rigs.broken_fk())
+	ik_button = cmds.iconTextButton(p=auto_rigs_row, l='IK', ann='Creates an IK rig. Select first joint',
+									style='iconAndTextVertical', image1='kinHandle.png',
+									command=lambda *args: auto_rigs.simple_ik())
+	rk_button = cmds.iconTextButton(p=auto_rigs_row, l='RK', ann='Creates a RK rig. Select first joint',
+									style='iconAndTextVertical', image1='kinJoint.png',
+									command=lambda *args: auto_rigs.simple_rk())
+
+	cmds.separator(p=tab_1_column, height=12)
+
+	# Skinning
+	bind_skin_button = cmds.button(p=tab_1_column, l='Bind Skin', ann='Binds skin to joint(s)',
+									command=lambda *args: cmds.bindSkin())
+	paint_weights_button = cmds.button(p=tab_1_column, l='Paint Weights', ann='Opens paint weight options',
+									command=lambda *args: mel.eval('ArtPaintSkinWeightsToolOptions'))
 
 	# tab 2
 	tab_2_column = cmds.columnLayout(adjustableColumn=True, columnAlign='center', parent=my_tabs)
 
-	cmds.separator(height=24, parent=tab_1_column)
-	other_label = cmds.text(label='BETA Functions Below!', parent=tab_1_column)
-	cmds.separator(height=24, parent=tab_1_column)
+	cmds.separator(p=tab_1_column, height=24)
+	other_label = cmds.text(p=tab_1_column, l='BETA Functions Below!')
+	cmds.separator(p=tab_1_column, height=24)
 
 	# Random Placement
-	random_placement_button = cmds.button(parent=tab_1_column, label='Random Placement', annotation='Random placement options', command=lambda *args: random_placement.random_placement_prompt())
+	random_placement_button = cmds.button(p=tab_1_column, l='Random Placement', ann='Random placement options',
+										  command=lambda *args: random_placement.random_placement_prompt())
 
 	# Custom Outliner
-	custom_outliner_button = cmds.button(parent=tab_1_column, label='Custom Outliner', annotation='Open custom outliner', command=lambda *args: custom_outliner.custom_outliner())
+	custom_outliner_button = cmds.button(p=tab_1_column, l='Custom Outliner', ann='Open custom outliner',
+										 command=lambda *args: custom_outliner.custom_outliner())
 
 	# Custom Animation window
-	custom_outliner_button = cmds.button(parent=tab_1_column, label='Animator', annotation='Open animation master', command=lambda *args: custom_animation_window.custom_animation())
+	custom_outliner_button = cmds.button(p=tab_1_column, l='Animator', ann='Open animation master',
+										 command=lambda *args: custom_animation_window.custom_animation())
 
 	# Add tabs
 	cmds.tabLayout(my_tabs, edit=True, tabLabel=[(tab_1_column, 'Main'), (tab_2_column, '...')])
@@ -142,12 +203,10 @@ def create_my_toolbox(standalone=False):
 	# cmds.showWindow(my_window)
 
 	# Check if dockable window exists
-	if cmds.dockControl('CSA Toolbox', query=True, exists=True):
-		cmds.deleteUI('CSA Toolbox', control=True)
-	elif cmds.dockControl('MayaWindow|CSA_Toolbox', query=True, exists=True):
-		cmds.deleteUI('MayaWindow|CSA_Toolbox')
+	my_dock_control='CSAToolbox'
+	if cmds.dockControl(my_dock_control, q=True, exists=True):
+		cmds.deleteUI(my_dock_control, control=True)
 
 	# Show as dockable window
 	allowedAreas = ['right', 'left']
-	my_dock_control = cmds.dockControl('CSA Toolbox', area='left', aa=allowedAreas, content=my_window)
-	print(my_dock_control)
+	my_dock_control = cmds.dockControl(my_dock_control, area='left', aa=allowedAreas, content=my_window)
