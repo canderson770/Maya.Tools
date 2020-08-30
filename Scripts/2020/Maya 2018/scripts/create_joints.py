59097982f361fa62123e0a294fbaacdae5320ef0
Unknown <rp17000@gmail.com>
2020-08-30 03:19:58 -0600

    changed value name

--- Scripts/2020/Maya 2018/scripts/create_joints.py
+++ Scripts/2020/Maya 2018/scripts/create_joints.py
@@ -8,14 +8,14 @@ def convert_to_joints(checkbox):
     '''Creates joints at selection. Will replace selection if the parameter is True'''
     sels = cmds.ls(sl=True)
     last_jnt = ''
-    replace = cmds.checkBox(checkbox, q=True, value=True)
+    value = cmds.checkBox(checkbox, q=True, value=True)
 
     for sel in sels:
         cmds.select(clear=True)
         pos = cmds.xform(sel, q=True, rotatePivot=True, ws=True)
         jnt = cmds.joint(position=pos, absolute=True)
         
-        if replace == True:
+        if value == True:
             cmds.delete(sel)
         
         if last_jnt != '':
