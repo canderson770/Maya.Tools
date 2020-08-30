1d030d6dd25cc0349937842b700adbbf16d6a117
Unknown <rp17000@gmail.com>
2020-08-30 03:26:36 -0600

    clean up

--- Scripts/2020/Maya 2018/scripts/simple_functions.py
+++ Scripts/2020/Maya 2018/scripts/simple_functions.py
@@ -7,16 +7,15 @@ import maya.mel as mel
 
 def delete_history():
     cmds.delete(ch=True)
-    
+
+
 def freeze_transformations():
     cmds.makeIdentity(apply=True)
-    
+
+
 def center_pivot():
     cmds.xform(centerPivots=True)
-    
-def parent_scale_constrain():
-    cmds.parentConstraint()
-    cmds.scaleConstraint()
-    
+
+
 def toggle_poly_count():
     mel.eval('TogglePolyCount')
